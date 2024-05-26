b31874825a1be9e6728074cf786ff3e6407a3b25
paakwesi <brokingsley715@gmail.com>
2024-05-26 03:06:20 +0000

    another lesson learnt @that moment

--- Practice.java
+++ Practice.java
@@ -1,8 +1,19 @@
-public class Array {
+public class Practice {
+    // the addNumber is the instance of the class Practice and a method
+    int addNumbers( int a, int b){
+        int product = a * b;
+                return product;
+
+                // method overriding
+
+    };
     public static void main(String[]args){
+
+        // ways of defining arrays
         int[]  age = {23, 45,21,65,87};
 
         int i;
+
         for(i=0;i <age.length; i++){
             System.out.println(age[i]);
         }
@@ -15,6 +26,11 @@ public class Array {
             System.out.println(names[1]);
             System.out.println(names[2]);
             System.out.println(names[3]);
+
+            String[] name ={"wisdom", "kelly","Sarpong"};
+            System.out.println(name[0]);
+            Practice obj = new Practice();
+            System.out.println(obj.addNumbers(5,6));
 
 
 
